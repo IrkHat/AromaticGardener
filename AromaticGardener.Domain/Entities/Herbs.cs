208b97f59b85884a4bf293436e22c2e453da44ad
IrkHat <kithar@live.com.pt>
2024-04-19 14:59:09 +0100

    Classes

---
+++ AromaticGardener.Domain/Entities/Herbs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AromaticGardener.Domain.Entities
+{
+    public class Herbs
+    {
+        public int Id { get; set; }
+        public required string CommonName { get; set; }
+        public string? Description { get; set; }
+        public string? BestSoilType { get; set; }
+        public required double SoilPhMin { get; set; }
+        public required double SoilPhMax { get; set; }
+        public required string Watering { get; set; }
+        public required string Insulation { get; set; }
+        public required string GrowthHabit { get; set; }
+        public required string LifeCycle { get; set; }
+        public required string Kingdom { get; set; }
+        public string? Subkingdom { get; set; }
+        public string? Superdivision { get; set; }
+        public required string Division { get; set; }
+        public required string Class { get; set; }
+        public string? Subclass { get; set; }
+        public required string Order { get; set; }
+        public required string Family { get; set; }
+        public required string Genus { get; set; }
+        public required string Species { get; set; }
+        public  string? ImageUrl { get; set; }
+
+
+    }
+}
